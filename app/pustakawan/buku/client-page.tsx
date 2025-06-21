3c1282cb61806adb31d3097eb21093c177126c34
khauraqila <111202314878@mhs.dinus.ac.id>
2025-06-21 14:45:38 +0700

    'okok'

--- app/pustakawan/buku/client-page.tsx
+++ app/pustakawan/buku/client-page.tsx
@@ -4,13 +4,12 @@ import { useState, useEffect } from "react"
 import { Search, Plus } from 'lucide-react'
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { DataTableBuku } from "@/app/_components/pustakawan/buku/data-table-buku"
-import { AddBukuDialog } from "@/app/_components/pustakawan/buku/add-buku-dialog"
-import { EditBukuDialog } from "@/app/_components/pustakawan/buku/edit-buku-dialog"
-import { DeleteBukuDialog } from "@/app/_components/pustakawan/buku/delete-buku-dialog"
-import { columnsBuku, type DataBuku } from "@/app/_components/pustakawan/buku/columns-buku"
+import { DataTableBuku } from "@/app/_components/admin/buku/data-table-buku"
+import { AddBukuDialog } from "@/app/_components/admin/buku/add-buku-dialog"
+import { EditBukuDialog } from "@/app/_components/admin/buku/edit-buku-dialog"
+import { DeleteBukuDialog } from "@/app/_components/admin/buku/delete-buku-dialog"
+import { columnsBuku, type DataBuku } from "@/app/_components/admin/buku/columns-buku"
 import { DynamicBreadcrumb } from "@/app/_components/breadcrumb"
-
 
 export default function ClientManajemenBuku() {
   const [searchQuery, setSearchQuery] = useState("")
@@ -211,11 +210,10 @@ export default function ClientManajemenBuku() {
   return (
     <div className="flex flex-col min-h-screen bg-[#D9DBF3] text-[#0E4D97]">
       <main className="flex-1 p-6">
-
         <div className="mb-2">
           <DynamicBreadcrumb />
         </div>
-
+        
         {/* Header */}
         <div className="mb-6">
           {isLoading ? (
